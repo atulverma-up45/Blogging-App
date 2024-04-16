59c815ab6e9e59f6a69ff4c38f62a3465b1ebae6
AtulVerma_up45 <AtulVerma04680@gmail.com>
2024-04-16 18:42:21 +0530

    user controllers for blogging app

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,10 @@
 import User from "../models/user.models.js";
 import signUpUserTemplate from "../mail/Templates/signUpUser.templates.js";
 import changePasswordTemplate from "../mail/Templates/changePassword.mailTemplates.js";
-
 import otpGenerator from "otp-generator";
 import sendMail from "../utils/sendMail.js";
+import { nanoid } from "nanoid";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 // email validation function
 function isValidEmail(email) {
@@ -77,6 +78,20 @@ export const sigUpController = async (req, res) => {
       });
     }
 
+    const generateUsername = async (email) => {
+      let username = email.split("@")[0];
+      username = username.replace(/[^a-zA-Z0-9]/g, "");
+      let isUsernameNotUnique = await User.exists({
+        "personal_info.username": username,
+      }).then((result) => {
+        return result;
+      });
+
+      return isUsernameNotUnique
+        ? (username += nanoid().substring(0, 5))
+        : username;
+    };
+
     // Create the user
     const createdUser = await User.create({
       personal_info: {
@@ -86,6 +101,7 @@ export const sigUpController = async (req, res) => {
         password: confirmPassword,
         contactNumber,
         avatar: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
+        username: await generateUsername(email),
       },
     });
 
@@ -107,19 +123,19 @@ export const sigUpController = async (req, res) => {
 
     // Get the logged-in user without sensitive information
     const loggedInUser = await User.findById(createdUser._id).select(
-      "-password -refreshToken"
+      "-personal_info.password -refreshToken -social_links -google_auth -accountType -joinedAt -updatedAt -account_info -blogs -__v -personal_info.contactNumber -_id "
     );
 
-      // Set the JWT as a cookie and create options
-      const options = {
-        httpOnly: true,
-        sameSite: "Strict",
-        secure: true,
-      };
+    // Set the JWT as a cookie and create options
+    const options = {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: true,
+    };
 
-      // Include the access token and Refresh token in the response headers
-      res.setHeader("X-accessToken", accessToken);
-      res.setHeader("X-refreshToken", refreshToken);
+    // Include the access token and Refresh token in the response headers
+    res.setHeader("X-accessToken", accessToken);
+    res.setHeader("X-refreshToken", refreshToken);
 
     // Return response
     return res
@@ -130,6 +146,7 @@ export const sigUpController = async (req, res) => {
         success: true,
         message: "The user was successfully registered.",
         accessToken: accessToken,
+        user: loggedInUser.personal_info,
       });
   } catch (error) {
     console.error("Error during user registration:", error);
@@ -190,6 +207,11 @@ export const loginController = async (req, res) => {
       user._id
     );
 
+    // Get the logged-in user without sensitive information
+    const loggedInUser = await User.findById(user._id).select(
+      "-personal_info.password -refreshToken -social_links -google_auth -accountType -joinedAt -updatedAt -account_info -blogs -__v -personal_info.contactNumber -_id "
+    );
+
     // // Get the logged-in user without sensitive information
     // const loggedInUser = await User.findById(user._id).select(
     //   "-password -refreshToken"
@@ -215,6 +237,7 @@ export const loginController = async (req, res) => {
         success: true,
         message: "User logged in successfully",
         accessToken: accessToken,
+        user: loggedInUser.personal_info,
       });
   } catch (error) {
     console.log("Error Occurred While Login", error);
@@ -229,16 +252,29 @@ export const loginController = async (req, res) => {
 export const changePasswordController = async (req, res) => {
   try {
     // fetch the old password and new password and confirm password from req body
-    const { oldPassword, newPassword, confirmPassword } = req.body;
+    const { currentPassword, newPassword } = req.body;
 
     // fetch email from req
     const email = req.user.personal_info.email;
 
     // validate the data
-    if (!oldPassword || !newPassword || !confirmPassword) {
+    if (!currentPassword.length || !newPassword.length) {
       return res.status(400).json({
         success: false,
         message: "All fields are Required",
+      });
+    }
+
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+    if (
+      !passwordRegex.test(currentPassword) ||
+      !passwordRegex.test(newPassword)
+    ) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters",
       });
     }
 
@@ -256,26 +292,18 @@ export const changePasswordController = async (req, res) => {
     }
 
     // check old password is correct or not
-    const isCorrectPassword = await user.isPasswordCorrect(oldPassword);
+    const isCorrectPassword = await user.isPasswordCorrect(currentPassword);
 
     if (!isCorrectPassword) {
       return res.status(401).json({
         success: false,
         message:
-          "Old Password is incorrect. Please enter the valid Old password",
-      });
-    }
-
-    // validate the new password and confirm password
-    if (newPassword !== confirmPassword) {
-      return res.status(400).json({
-        success: false,
-        message: "New Password and Confirm Password Should Be Same",
+          "Current Password is incorrect. Please enter the valid Current password",
       });
     }
 
     // Update the password in the user instance (not saved to the database yet)
-    user.personal_info.password = confirmPassword;
+    user.personal_info.password = newPassword;
 
     // Save the updated user to the database
     await user.save();
@@ -284,7 +312,7 @@ export const changePasswordController = async (req, res) => {
     await sendMail(
       user.personal_info.email,
       "Your Password Changed Successfully Mail From Developer Atul Verma",
-      changePasswordTemplate(user.personal_info.firstName, confirmPassword)
+      changePasswordTemplate(user.personal_info.firstName, newPassword)
     );
 
     return res.status(200).json({
@@ -305,6 +333,8 @@ export const userDetailsController = async (req, res) => {
   try {
     // fetch the user id
     const userId = req.user._id;
+
+    console.log(userId);
 
     // validate the user id
     if (!userId) {
@@ -392,3 +422,113 @@ export const logoutUserContoller = async (req, res) => {
     });
   }
 };
+
+// update profile image with img url
+export const updateProfileImageController = async (req, res) => {
+  try {
+    // destructure the data from req
+    const avatar = req.file?.path;
+    const user_id = req.user._id;
+
+    if (!avatar) {
+      return res.status(400).json({
+        success: false,
+        message: "Avatar is required.",
+      });
+    }
+
+    const response = await uploadOnCloudinary(avatar);
+
+    const url = response.secure_url;
+
+    const profile = await User.findOneAndUpdate(
+      { _id: user_id },
+      { "personal_info.avatar": url }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Profile image Updated Successfully",
+      avatar: url,
+    });
+  } catch (error) {
+    console.log(
+      "Error occured while updating the profile image : updateProfileImageController",
+      error
+    );
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const updateProfileController = async (req, res) => {
+  // destructure the data from the request
+  const { username, bio, social_links } = req.body;
+  const user_id = req.user._id;
+
+  const bioLimit = 150;
+
+  if (username.length < 3) {
+    return res.status(400).json({
+      success: false,
+      message: "Username should be at least 3 letters long",
+    });
+  }
+
+  if (bio.length > bioLimit) {
+    return res.status(400).json({
+      success: false,
+      message: `Bio should not be more than ${bioLimit} characters`,
+    });
+  }
+
+  try {
+    for (const [key, value] of Object.entries(social_links)) {
+      if (value.length > 0) {
+        const hostname = new URL(value).hostname;
+        const domain = `${key}.com`;
+        if (!hostname.includes(domain) && key !== "website") {
+          return res.status(400).json({
+            success: false,
+            message: `${key} link is invalid. You must enter a full link`,
+          });
+        }
+      }
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Invalid social links. Please provide full links with 'https' included",
+    });
+  }
+
+  const updateObj = {
+    "personal_info.username": username,
+    "personal_info.bio": bio,
+    social_links,
+  };
+
+  try {
+    await User.findOneAndUpdate({ _id: user_id }, updateObj, { runValidators: true });
+    return res.status(200).json({
+      success: true,
+      message: "User Profile Updated Successfully",
+      username,
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(409).json({
+        message: "Username is already taken",
+        success: false,
+      });
+    }
+    console.error("Error Occurred in Update Profile Controller:", error.message);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
+
